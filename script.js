ac6d89a8075baeb2f0ee824f9333d77da775af72
Yara Rabie Safan <63198967+y-rabie12@users.noreply.github.com>
2022-04-30 11:10:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,78 +11,104 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map = '';
+let map,mapEvent;
 
-let mapEvent;
+
+class App {
+
+    #map;
+    #mapEvent;
+
+    
+    constructor(){
+       this._getPositon();
+
+       form.addEventListener('submit',this._newWorkout.bind(this))
+
+       inputType.addEventListener('change', this._toggleElevationField)
+    
+    }
+
+    _getPositon(){
+        navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+            alert('Could not get the current location! Try Again')
+        })
+
+    }
+
+    _loadMap(position){
+            // Destructure
+            const { latitude } = position.coords
+            const { longitude } = position.coords
+        
+            const coordinates = [latitude,longitude]
+            console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+        
+        
+            // Pass in the coordinates ,  zoom level of the map
+             this.#map = L.map('map').setView(coordinates, 13);
+            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(this.#map);
+        
+           
+        
+        
+            // The 'on' method is used by the map object ,  it is from the leaflet library not from JavaScript. 
+            // Pass in the event type and the callback function   
+        
+        
+            this.#map.on('click', this._showForm.bind(this))
+        
+
+    }
+
+    _showForm(mapE){
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+
+    _toggleElevationField(){
+        
+            inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+            inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    
+    }
+
+    _newWorkout(e){
+        e.preventDefault();
+
+        // Clear all fields
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
+ 
+        const {lat,lng} = this.#mapEvent.latlng
+        L.marker([lat,lng]).addTo(this.#map)
+        .bindPopup(L.popup({
+            maxWidth:250,
+            minWidth:100,
+            autoClose: false,
+            closeOnClick:false,
+            className: 'running-popup'
+        })).setPopupContent('Running Activity')
+        .openPopup();
+    }
+}
+
+const app =  new App();
+
+
+
+
+
+
+
+
 
 // To start using the geolocation API 
 
 // This method accepts two callback functions , one for rejection and one for success
 
-
-navigator.geolocation.getCurrentPosition(function (position) {
-
-    // Destructure
-    const { latitude } = position.coords
-    const { longitude } = position.coords
-
-    const coordinates = [latitude,longitude]
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
-
-
-    // Pass in the coordinates ,  zoom level of the map
-     map = L.map('map').setView(coordinates, 13);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-   
-
-
-    // The 'on' method is used by the map object ,  it is from the leaflet library not from JavaScript. 
-    // Pass in the event type and the callback function   
-
-
-    map.on('click', function(mapE){
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-   
-      
-    })
-
-
-}, function () {
-    alert('Could not get the current location! Try Again')
-})
-
-
-
 // Sidenote: our script will have access to all global variables declared in the scripts loaded before it.
 
 // Sidenote: We want to display a pop up marker, however using addEventListener won't help , because we need to get the specific place where the user clicked.
-
-form.addEventListener('submit',function(e){
-       e.preventDefault();
-
-       // Clear all fields
-       inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
-
-       const {lat,lng} = mapEvent.latlng
-       L.marker([lat,lng]).addTo(map)
-       .bindPopup(L.popup({
-           maxWidth:250,
-           minWidth:100,
-           autoClose: false,
-           closeOnClick:false,
-           className: 'running-popup'
-       })).setPopupContent('Running Activity')
-       .openPopup();
-})
-
-
-inputType.addEventListener('change',function(){
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-})
